90e8af869f0e003ea8ba38e0401abfbf2b7c5bc6
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2021-11-15 19:16:44 -0500

    Added menu

---
+++ todo_list.py
@@ -0,0 +1,14 @@
+
+while True:
+    user_select = input("Press 1 to add task\nPress 2 to delete task\nPress 3 to view all tasks\nPress q to quit\n\n")
+
+    if user_select.lower() == "1":
+        break
+    elif user_select.lower() == "2":
+        break
+    elif user_select.lower() == "3":
+        break
+    elif user_select.lower() == "q":
+        break
+    else:
+        print("\nInvalid option. Try again...\n")
